a50d258655c9c69a01e2f91a090289eeb4ee0aa4
isotaan <42243583+isotaan@users.noreply.github.com>
2023-07-28 15:56:47 -0400

    Updated CSG8F functions

---
+++ etc/CSG8_CRNGE_SiteSpawner.01.0006.lua
@@ -0,0 +1,60 @@
+
+--Strategic Sites (no canyon sites)
+CSG8F.spawn.spawnStaticTemplate({zone = "SPAWN_FOB-1", staticTemplate = CSG8_TEMPLATES.fobTemplates[1].statics})
+CSG8F.spawn.spawnStaticTemplate({zone = "SPAWN_FOB-2", staticTemplate = CSG8_TEMPLATES.fobTemplates[1].statics})
+CSG8F.spawn.spawnStaticTemplate({zone = "SPAWN_FOB-3", staticTemplate = CSG8_TEMPLATES.fobTemplates[1].statics})
+CSG8F.spawn.spawnStaticTemplate({zone = "SPAWN_FOB-4", staticTemplate = CSG8_TEMPLATES.fobTemplates[1].statics})
+CSG8F.spawn.spawnStaticTemplate({zone = "SPAWN_FOB-5", staticTemplate = CSG8_TEMPLATES.fobTemplates[1].statics})
+CSG8F.spawn.spawnStaticTemplate({zone = "SPAWN_FOB-6", staticTemplate = CSG8_TEMPLATES.fobTemplates[1].statics})
+CSG8F.spawn.spawnStaticTemplate({zone = "SPAWN_FOB-7", staticTemplate = CSG8_TEMPLATES.fobTemplates[1].statics})
+CSG8F.spawn.spawnStaticTemplate({zone = "SPAWN_FOB-8", staticTemplate = CSG8_TEMPLATES.fobTemplates[1].statics})
+
+--Northeast SAMs
+CSG8F.crnge.spawn.spawnSAMandTemplateZone({zone = "SPAWN_SAM_SA2", groupTemplate = "SA-2 SAM Site MAD", staticTemplate = CSG8_TEMPLATES.samTemplates[2].statics,verbose = false})
+CSG8F.crnge.spawn.spawnSAMandTemplateZone({zone = "SPAWN_SAM_SA2-1", groupTemplate = "SA-2 SAM Site MAD", staticTemplate = CSG8_TEMPLATES.samTemplates[2].statics,verbose = false})
+CSG8F.crnge.spawn.spawnSAMandTemplateZone({zone = "SPAWN_SAM_SA2-2", groupTemplate = "SA-2 SAM Site MAD", staticTemplate = CSG8_TEMPLATES.samTemplates[2].statics,verbose = false})
+CSG8F.crnge.spawn.spawnSAMandTemplateZone({zone = "SPAWN_SAM_SA2-3", groupTemplate = "SA-2 SAM Site MAD", staticTemplate = CSG8_TEMPLATES.samTemplates[2].statics,verbose = false})
+CSG8F.crnge.spawn.spawnSAMandTemplateZone({zone = "SPAWN_SAM_SA2-4", groupTemplate = "SA-2 SAM Site MAD", staticTemplate = CSG8_TEMPLATES.samTemplates[2].statics,verbose = false})
+CSG8F.crnge.spawn.spawnSAMandTemplateZone({zone = "SPAWN_SAM_SA3", groupTemplate = "SA-3 SAM Site", staticTemplate = CSG8_TEMPLATES.samTemplates[3].statics,verbose = false})
+CSG8F.crnge.spawn.spawnSAMandTemplateZone({zone = "SPAWN_SAM_SA3-1", groupTemplate = "SA-3 SAM Site", staticTemplate = CSG8_TEMPLATES.samTemplates[3].statics,verbose = false})
+CSG8F.crnge.spawn.spawnSAMandTemplateZone({zone = "SPAWN_SAM_SA3-2", groupTemplate = "SA-3 SAM Site", staticTemplate = CSG8_TEMPLATES.samTemplates[3].statics,verbose = false})
+CSG8F.crnge.spawn.spawnSAMandTemplateZone({zone = "SPAWN_SAM_SA3-3", groupTemplate = "SA-3 SAM Site", staticTemplate = CSG8_TEMPLATES.samTemplates[3].statics,verbose = false})
+CSG8F.crnge.spawn.spawnSAMandTemplateZone({zone = "SPAWN_SAM_SA5", groupTemplate = "SA-5 SAM Site", staticTemplate = CSG8_TEMPLATES.samTemplates[1].statics,verbose = false})
+
+--WW2 Range Spawns
+CSG8F.crnge.spawn.spawnSAMandTemplateZone({zone = "SPAWN_AAA_BOF", groupTemplate = "Bofors Battery", staticTemplate = CSG8_TEMPLATES.aaaTemplates[1].statics,verbose = false})
+CSG8F.crnge.spawn.spawnSAMandTemplateZone({zone = "SPAWN_AAA_BOF-1", groupTemplate = "Bofors Battery", staticTemplate = CSG8_TEMPLATES.aaaTemplates[1].statics,verbose = false})
+CSG8F.crnge.spawn.spawnSAMandTemplateZone({zone = "SPAWN_AAA_BOF-2", groupTemplate = "Bofors Battery", staticTemplate = CSG8_TEMPLATES.aaaTemplates[1].statics,verbose = false})
+CSG8F.crnge.spawn.spawnSAMandTemplateZone({zone = "SPAWN_AAA_BOF-3", groupTemplate = "Bofors Battery", staticTemplate = CSG8_TEMPLATES.aaaTemplates[1].statics,verbose = false})
+CSG8F.crnge.spawn.spawnSAMandTemplateZone({zone = "SPAWN_AAA_BOF-4", groupTemplate = "Bofors Battery", staticTemplate = CSG8_TEMPLATES.aaaTemplates[1].statics,verbose = false})
+CSG8F.crnge.spawn.spawnSAMandTemplateZone({zone = "SPAWN_AAA_FLAK88", groupTemplate = "Flak 88 Battery", staticTemplate = CSG8_TEMPLATES.aaaTemplates[1].statics,verbose = false})
+CSG8F.crnge.spawn.spawnSAMandTemplateZone({zone = "SPAWN_AAA_FLAK88-1", groupTemplate = "Flak 88 Battery", staticTemplate = CSG8_TEMPLATES.aaaTemplates[1].statics,verbose = false})
+
+--Northwestern SAMs
+CSG8F.crnge.spawn.spawnSAMandTemplateZone({zone = "SPAWN_SAM_SA20", groupTemplate = "SA-20B SAM Site",verbose = false})
+CSG8F.crnge.spawn.spawnSAMandTemplateZone({zone = "SPAWN_SAM_SA17", groupTemplate = "SA-17 SAM Battery",verbose = false})
+CSG8F.crnge.spawn.spawnSAMandTemplateZone({zone = "SPAWN_SAM_SA17-1", groupTemplate = "SA-17 SAM Battery",verbose = false})
+CSG8F.crnge.spawn.spawnSAMandTemplateZone({zone = "SPAWN_SAM_SA17-2", groupTemplate = "SA-17 SAM Battery",verbose = false})
+CSG8F.crnge.spawn.spawnSAMandTemplateZone({zone = "SPAWN_SAM_SA17-3", groupTemplate = "SA-17 SAM Battery",verbose = false})
+CSG8F.crnge.spawn.spawnSAMandTemplateZone({zone = "SPAWN_SAM_SA10B", groupTemplate = "SA-10B SAM Site",verbose = false})
+CSG8F.crnge.spawn.spawnSAMandTemplateZone({zone = "SPAWN_SAM_SA19", groupTemplate = "SA-19 SAM Battery",verbose = false})
+
+-- Generic Strat Sites
+CSG8F.spawn.spawnStaticTemplate({zone = "SPAWN_STRATSITE", staticTemplate = CSG8_TEMPLATES.fobTemplates[2].statics})
+CSG8F.spawn.spawnStaticTemplate({zone = "SPAWN_STRATSITE-1", staticTemplate = CSG8_TEMPLATES.fobTemplates[2].statics})
+CSG8F.spawn.spawnStaticTemplate({zone = "SPAWN_STRATSITE-2", staticTemplate = CSG8_TEMPLATES.fobTemplates[2].statics})
+CSG8F.spawn.spawnStaticTemplate({zone = "SPAWN_STRATSITE-3", staticTemplate = CSG8_TEMPLATES.fobTemplates[2].statics})
+CSG8F.spawn.spawnStaticTemplate({zone = "SPAWN_STRATSITE-4", staticTemplate = CSG8_TEMPLATES.fobTemplates[2].statics})
+CSG8F.spawn.spawnStaticTemplate({zone = "SPAWN_STRATSITE-5", staticTemplate = CSG8_TEMPLATES.fobTemplates[2].statics})
+
+
+--Eastern Canyon Range
+CSG8F.spawn.spawnStaticTemplate({zone = "SPAWN_STRATSITE-6", staticTemplate = CSG8_TEMPLATES.fobTemplates[2].statics, spawnPosition = mist.utils.makeVec2(mist.utils.zoneToVec3("SPAWN_STRATSITE-6")) })
+CSG8F.spawn.spawnStaticTemplate({zone = "SPAWN_STRATSITE-7", staticTemplate = CSG8_TEMPLATES.fobTemplates[2].statics, spawnPosition = mist.utils.makeVec2(mist.utils.zoneToVec3("SPAWN_STRATSITE-7")) })
+CSG8F.crnge.spawn.spawnGroupandTemplateZone({zone = "MANPADS_SA18", groupTemplate = "SA-18 MANPADS Detachment", random = false,verbose = false})
+CSG8F.crnge.spawn.spawnGroupandTemplateZone({zone = "MANPADS_SA18-1", groupTemplate = "SA-18 MANPADS Detachment", random = false,verbose = false})
+CSG8F.crnge.spawn.spawnGroupandTemplateZone({zone = "MANPADS_SA18-2", groupTemplate = "SA-18 MANPADS Detachment", random = false,verbose = false})
+CSG8F.crnge.spawn.spawnGroupandTemplateZone({zone = "MANPADS_SA18-3", groupTemplate = "SA-18 MANPADS Detachment", random = false,verbose = false})
+CSG8F.crnge.spawn.spawnSAMandTemplateZone({zone = "SPAWN_SAM_SA8", groupTemplate = "SA-8 SAM Section",verbose = false})
+CSG8F.crnge.spawn.spawnSAMandTemplateZone({zone = "SPAWN_SAM_SA8-1", groupTemplate = "SA-8 SAM Section",verbose = false})
+CSG8F.crnge.spawn.spawnSAMandTemplateZone({zone = "SPAWN_SAM_SA6", groupTemplate = "SA-6 SAM Battery",verbose = false})
